fc5bcc54a20fb287e568172af2141967ed3c4c93
Igboanugwo Collins <munisco12@gmail.com>
2022-08-22 14:18:19 +0100

    working on the signup page

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -173,7 +173,7 @@ const Register = ({ providers }: LoginProps) => {
                         />
                     </div>
 
-                    <h1 className='heroText text-[22px] md:text-[96px] lg:text- absolute top-[6rem] left-[5rem] absolute uppercase'>
+                    <h1 className='heroText text-[22px] md:text-[5rem] absolute top-[6rem] left-[5rem] absolute uppercase'>
                         Shoot for the stars
                     </h1>
                 </div>
