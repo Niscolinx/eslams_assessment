f64c80c0e761c133df76584afc56a13878cc9767
Igboanugwo Collins <munisco12@gmail.com>
2022-08-22 08:25:16 +0100

    initial setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { useEffect } from 'react'
 
 export default function Document() {
     return (
         <Html>
             <Head />
-            <title>1960token</title>
+            <title>Eslams</title>
             <body>
                 <Main />
                 <NextScript />
-                {/* <script>
-                         (adsbygoogle = window.adsbygoogle || []).push({});
-                </script> */}
+              
             </body>
         </Html>
     )
