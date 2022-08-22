02407f331a154afd7bb00cad073f1dc091b07a76
Igboanugwo Collins <munisco12@gmail.com>
2022-08-22 11:39:26 +0100

    working on the signup page

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -142,7 +142,11 @@ const Register = ({ providers }: LoginProps) => {
 
             <div className='grid'>
                 <div className='section-1'>
-                    <Image src='/hero-player.png' width='661px' height='100%' />
+                    <Image
+                        src='/hero-player.png'
+                        width='100%'
+                        height='661px'
+                    />
                     <h1 className='heroText'>Shoot for the stars</h1>
                 </div>
 
