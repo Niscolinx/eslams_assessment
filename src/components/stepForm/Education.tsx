7837fec2b12d9522e97854aa26221509068b75da
Igboanugwo Collins <munisco12@gmail.com>
2022-08-23 10:39:42 +0100

    working on signup

--- src/components/stepForm/Education.tsx
+++ src/components/stepForm/Education.tsx
@@ -1,92 +1,82 @@
 import * as React from 'react'
-import Typography from '@mui/material/Typography'
-import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import ListItemText from '@mui/material/ListItemText'
 import Grid from '@mui/material/Grid'
+import TextField from '@mui/material/TextField'
+import FormControlLabel from '@mui/material/FormControlLabel'
 
-const products = [
-    {
-        name: 'Product 1',
-        desc: 'A nice thing',
-        price: '$9.99',
-    },
-    {
-        name: 'Product 2',
-        desc: 'Another thing',
-        price: '$3.45',
-    },
-    {
-        name: 'Product 3',
-        desc: 'Something else',
-        price: '$6.51',
-    },
-    {
-        name: 'Product 4',
-        desc: 'Best thing of all',
-        price: '$14.11',
-    },
-    { name: 'Shipping', desc: '', price: 'Free' },
-]
-const addresses = ['1 MUI Drive', 'Reactville', 'Anytown', '99999', 'USA']
-const payments = [
-    { name: 'Card type', detail: 'Visa' },
-    { name: 'Card holder', detail: 'Mr John Smith' },
-    { name: 'Card number', detail: 'xxxx-xxxx-xxxx-1234' },
-    { name: 'Expiry date', detail: '04/2024' },
-]
+import {
+    FormControl,
+    FormLabel,
+    RadioGroup,
+    Radio,
+    InputLabel,
+    MenuItem,
+    Select,
+} from '@mui/material'
+import { useState } from 'react'
+import MuiPhoneNumber from 'material-ui-phone-number'
 
 export default function Education() {
+    const [phoneNumber, setPhoneNumber] = useState<
+        HTMLInputElement | undefined
+    >()
+    const [relationship, setRelationship] = useState('')
+
     return (
         <React.Fragment>
-            <Typography variant='h6' gutterBottom>
-                Order summary
-            </Typography>
-            <List disablePadding>
-                {products.map((product) => (
-                    <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
-                        <ListItemText
-                            primary={product.name}
-                            secondary={product.desc}
-                        />
-                        <Typography variant='body2'>{product.price}</Typography>
-                    </ListItem>
-                ))}
-                <ListItem sx={{ py: 1, px: 0 }}>
-                    <ListItemText primary='Total' />
-                    <Typography variant='subtitle1' sx={{ fontWeight: 700 }}>
-                        $34.06
-                    </Typography>
-                </ListItem>
-            </List>
-            <Grid container spacing={2}>
+            {/* <Typography variant='h6' gutterBottom>
+                Shipping address
+            </Typography> */}
+            <Grid container spacing={3}>
                 <Grid item xs={12} sm={6}>
-                    <Typography variant='h6' gutterBottom sx={{ mt: 2 }}>
-                        Shipping
-                    </Typography>
-                    <Typography gutterBottom>John Smith</Typography>
-                    <Typography gutterBottom>{addresses.join(', ')}</Typography>
+                    <TextField
+                        required
+                        id='name'
+                        name='name'
+                        label='Name'
+                        fullWidth
+                        autoComplete='family-name'
+                        variant='outlined'
+                    />
                 </Grid>
-                <Grid item container direction='column' xs={12} sm={6}>
-                    <Typography variant='h6' gutterBottom sx={{ mt: 2 }}>
-                        Payment details
-                    </Typography>
-                    <Grid container>
-                        {payments.map((payment) => (
-                            <React.Fragment key={payment.name}>
-                                <Grid item xs={6}>
-                                    <Typography gutterBottom>
-                                        {payment.name}
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={6}>
-                                    <Typography gutterBottom>
-                                        {payment.detail}
-                                    </Typography>
-                                </Grid>
-                            </React.Fragment>
-                        ))}
-                    </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        required
+                        id='email'
+                        name='email'
+                        label='Email Address'
+                        fullWidth
+                        autoComplete='Email address'
+                        variant='outlined'
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                    <FormControl fullWidth>
+                        <InputLabel id='select-label'>Relationship</InputLabel>
+                        <Select
+                            labelId='select-label'
+                            id='select'
+                            value={relationship}
+                            label='Relationship'
+                            onChange={(e) => setRelationship(e.target.value)}
+                        >
+                            <MenuItem value={20}>Father</MenuItem>
+                            <MenuItem value={30}>Mother</MenuItem>
+                            <MenuItem value={10}>Guardian</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <MuiPhoneNumber
+                        id='phone-number'
+                        autoFocus={true}
+                        helperText='Enter phone number'
+                        // InputLabelProps={{ shrink: true, focused: true }}
+                        placeholder='Phone Number'
+                        defaultCountry={'us'}
+                        variant='outlined'
+                        onChange={(e: any) => setPhoneNumber(e)}
+                    />
                 </Grid>
             </Grid>
         </React.Fragment>
