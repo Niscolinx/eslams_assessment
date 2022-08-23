addb97934fd9b11fdcea0f9d83e90942a0c6eac2
Igboanugwo Collins <munisco12@gmail.com>
2022-08-23 18:35:58 +0100

    added images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,39 +3,10 @@ import { useTheme } from 'next-themes'
 
 import { GetSessionParams, getSession } from 'next-auth/react'
 
+import {BiNotepad} from 'react-icons/bi'
+
 const Index = () => {
-    const { theme } = useTheme()
-    // const [neuToUse, setNeuToUse] = useState<{}>()
-    //const [stepsNeu, setStepsNeu] = useState<{}>()
-
-    // useEffect(() => {
-    //     if (theme === 'dark') {
-    //         setNeuToUse({
-    //             background: `linear-gradient(145deg, #1c1c30, #171729)`,
-    //             boxShadow: `7px 7px 14px #161625,
-    //              -7px -7px 14px #1e1e35`,
-    //             borderRadius: '50px',
-    //         })
-
-    //         setStepsNeu({})
-    //     } else {
-    //         setNeuToUse({
-    //             background: `linear-gradient(145deg, #dadadf, #b8b8bb)`,
-    //             boxShadow: `7px 7px 14px #a5a5a8,
-    //          -7px -7px 14px #f3f3f8`,
-    //             borderRadius: '50px',
-    //             color: '#1a1a2d',
-    //         })
-
-    //         setStepsNeu({
-    //             background: `linear-gradient(145deg, #dadadf, #b8b8bb)`,
-    //             boxShadow: `7px 7px 14px #a5a5a8,
-    //          -7px -7px 14px #f3f3f8`,
-    //             borderRadius: '50px',
-    //             color: '#1a1a2d',
-    //         })
-    //     }
-    // }, [theme])
+   
 
     return (
         <div className='marketplace'>
