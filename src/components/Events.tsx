26d9fe8288e098b0b117768595bf5b0eb6d41bfe
Igboanugwo Collins <munisco12@gmail.com>
2022-08-23 21:37:03 +0100

    working on header

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -15,8 +15,8 @@ const Event = ({ imgSrc, title, description, amount, date }: EventProps) => {
             <figure>
                 <Image
                     src={imgSrc}
-                    width='300px'
-                    height='300px'
+                    width='100px'
+                    height='100px'
                     objectFit='cover'
                     className='event__img'
                 />
@@ -42,21 +42,21 @@ function Events() {
                 date={new Date().toLocaleString()}
             />
             <Event
-                imgSrc='/img/basket1.png'
+                imgSrc='/img/basket2.png'
                 title='Discover the best'
                 description='how are you doing'
                 amount={200}
                 date={new Date().toLocaleString()}
             />
             <Event
-                imgSrc='/img/basket1.png'
+                imgSrc='/img/basket3.png'
                 title='Discover the best'
                 description='how are you doing'
                 amount={200}
                 date={new Date().toLocaleString()}
             />
             <Event
-                imgSrc='/img/basket1.png'
+                imgSrc='/img/basket4.png'
                 title='Discover the best'
                 description='how are you doing'
                 amount={200}
