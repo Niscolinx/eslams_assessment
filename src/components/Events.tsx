273bdb0a63825c07d0826c74b5b52428e322b501
Igboanugwo Collins <munisco12@gmail.com>
2022-08-24 16:34:11 +0100

    working on cards

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -77,7 +77,7 @@ function Events() {
                 </div>
 
                 <div className='row'>
-                    <div className='col-1-of-3'>
+                   
                         <div className='card'>
                             <div className='card__side card__side--front'>
                                 <div className='card__picture card__picture--1'>
@@ -110,9 +110,9 @@ function Events() {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                  
 
-                    <div className='col-1-of-3'>
+                
                         <div className='card'>
                             <div className='card__side card__side--front'>
                                 <div className='card__picture card__picture--2'>
@@ -145,145 +145,142 @@ function Events() {
                                 </div>
                             </div>
                         </div>
+                    
+                        <div className='card'>
+                            <div className='card__side card__side--front'>
+                                <div className='card__picture card__picture--3'>
+                                    &nbsp;
+                                </div>
+                                <h4 className='card__heading'>
+                                    <span className='card__heading-span card__heading-span--3'>
+                                        The Snow Adventurer
+                                    </span>
+                                </h4>
+                                <div className='card__details'>
+                                    <ul>
+                                        <li>5 day tours</li>
+                                        <li>Up to 15 people</li>
+                                        <li>3 tour guides</li>
+                                        <li>Sleep in provided tents</li>
+                                        <li>Difficulty: hard</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className='card__side card__side--back card__side--back-3'>
+                                <div className='card__cta'>
+                                    <div className='card__price-box'>
+                                        <p className='card__price-only'>Only</p>
+                                        <p className='card__price-value'>$897</p>
+                                    </div>
+                                    <a href='#popup' className='btn btn--white'>
+                                        Book now!
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+          ]
+                   
+                        <div className='card'>
+                            <div className='card__side card__side--front'>
+                                <div className='card__picture card__picture--3'>
+                                    &nbsp;
+                                </div>
+                                <h4 className='card__heading'>
+                                    <span className='card__heading-span card__heading-span--3'>
+                                        The Snow Adventurer
+                                    </span>
+                                </h4>
+                                <div className='card__details'>
+                                    <ul>
+                                        <li>5 day tours</li>
+                                        <li>Up to 15 people</li>
+                                        <li>3 tour guides</li>
+                                        <li>Sleep in provided tents</li>
+                                        <li>Difficulty: hard</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className='card__side card__side--back card__side--back-3'>
+                                <div className='card__cta'>
+                                    <div className='card__price-box'>
+                                        <p className='card__price-only'>Only</p>
+                                        <p className='card__price-value'>$897</p>
+                                    </div>
+                                    <a href='#popup' className='btn btn--white'>
+                                        Book now!
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                   
+                
+                        <div className='card'>
+                            <div className='card__side card__side--front'>
+                                <div className='card__picture card__picture--3'>
+                                    &nbsp;
+                                </div>
+                                <h4 className='card__heading'>
+                                    <span className='card__heading-span card__heading-span--3'>
+                                        The Snow Adventurer
+                                    </span>
+                                </h4>
+                                <div className='card__details'>
+                                    <ul>
+                                        <li>5 day tours</li>
+                                        <li>Up to 15 people</li>
+                                        <li>3 tour guides</li>
+                                        <li>Sleep in provided tents</li>
+                                        <li>Difficulty: hard</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className='card__side card__side--back card__side--back-3'>
+                                <div className='card__cta'>
+                                    <div className='card__price-box'>
+                                        <p className='card__price-only'>Only</p>
+                                        <p className='card__price-value'>$897</p>
+                                    </div>
+                                    <a href='#popup' className='btn btn--white'>
+                                        Book now!
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                  
+                        <div className='card'>
+                            <div className='card__side card__side--front'>
+                                <div className='card__picture card__picture--3'>
+                                    &nbsp;
+                                </div>
+                                <h4 className='card__heading'>
+                                    <span className='card__heading-span card__heading-span--3'>
+                                        The Snow Adventurer
+                                    </span>
+                                </h4>
+                                <div className='card__details'>
+                                    <ul>
+                                        <li>5 day tours</li>
+                                        <li>Up to 15 people</li>
+                                        <li>3 tour guides</li>
+                                        <li>Sleep in provided tents</li>
+                                        <li>Difficulty: hard</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className='card__side card__side--back card__side--back-3'>
+                                <div className='card__cta'>
+                                    <div className='card__price-box'>
+                                        <p className='card__price-only'>Only</p>
+                                        <p className='card__price-value'>$897</p>
+                                    </div>
+                                    <a href='#popup' className='btn btn--white'>
+                                        Book now!
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
-                    <div className='col-1-of-3'>
-                        <div className='card'>
-                            <div className='card__side card__side--front'>
-                                <div className='card__picture card__picture--3'>
-                                    &nbsp;
-                                </div>
-                                <h4 className='card__heading'>
-                                    <span className='card__heading-span card__heading-span--3'>
-                                        The Snow Adventurer
-                                    </span>
-                                </h4>
-                                <div className='card__details'>
-                                    <ul>
-                                        <li>5 day tours</li>
-                                        <li>Up to 15 people</li>
-                                        <li>3 tour guides</li>
-                                        <li>Sleep in provided tents</li>
-                                        <li>Difficulty: hard</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className='card__side card__side--back card__side--back-3'>
-                                <div className='card__cta'>
-                                    <div className='card__price-box'>
-                                        <p className='card__price-only'>Only</p>
-                                        <p className='card__price-value'>$897</p>
-                                    </div>
-                                    <a href='#popup' className='btn btn--white'>
-                                        Book now!
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-1-of-3'>
-                        <div className='card'>
-                            <div className='card__side card__side--front'>
-                                <div className='card__picture card__picture--3'>
-                                    &nbsp;
-                                </div>
-                                <h4 className='card__heading'>
-                                    <span className='card__heading-span card__heading-span--3'>
-                                        The Snow Adventurer
-                                    </span>
-                                </h4>
-                                <div className='card__details'>
-                                    <ul>
-                                        <li>5 day tours</li>
-                                        <li>Up to 15 people</li>
-                                        <li>3 tour guides</li>
-                                        <li>Sleep in provided tents</li>
-                                        <li>Difficulty: hard</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className='card__side card__side--back card__side--back-3'>
-                                <div className='card__cta'>
-                                    <div className='card__price-box'>
-                                        <p className='card__price-only'>Only</p>
-                                        <p className='card__price-value'>$897</p>
-                                    </div>
-                                    <a href='#popup' className='btn btn--white'>
-                                        Book now!
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-1-of-3'>
-                        <div className='card'>
-                            <div className='card__side card__side--front'>
-                                <div className='card__picture card__picture--3'>
-                                    &nbsp;
-                                </div>
-                                <h4 className='card__heading'>
-                                    <span className='card__heading-span card__heading-span--3'>
-                                        The Snow Adventurer
-                                    </span>
-                                </h4>
-                                <div className='card__details'>
-                                    <ul>
-                                        <li>5 day tours</li>
-                                        <li>Up to 15 people</li>
-                                        <li>3 tour guides</li>
-                                        <li>Sleep in provided tents</li>
-                                        <li>Difficulty: hard</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className='card__side card__side--back card__side--back-3'>
-                                <div className='card__cta'>
-                                    <div className='card__price-box'>
-                                        <p className='card__price-only'>Only</p>
-                                        <p className='card__price-value'>$897</p>
-                                    </div>
-                                    <a href='#popup' className='btn btn--white'>
-                                        Book now!
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-1-of-3'>
-                        <div className='card'>
-                            <div className='card__side card__side--front'>
-                                <div className='card__picture card__picture--3'>
-                                    &nbsp;
-                                </div>
-                                <h4 className='card__heading'>
-                                    <span className='card__heading-span card__heading-span--3'>
-                                        The Snow Adventurer
-                                    </span>
-                                </h4>
-                                <div className='card__details'>
-                                    <ul>
-                                        <li>5 day tours</li>
-                                        <li>Up to 15 people</li>
-                                        <li>3 tour guides</li>
-                                        <li>Sleep in provided tents</li>
-                                        <li>Difficulty: hard</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className='card__side card__side--back card__side--back-3'>
-                                <div className='card__cta'>
-                                    <div className='card__price-box'>
-                                        <p className='card__price-only'>Only</p>
-                                        <p className='card__price-value'>$897</p>
-                                    </div>
-                                    <a href='#popup' className='btn btn--white'>
-                                        Book now!
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
                 <div className='u-center-text u-margin-top-huge'>
                     <a href='#' className='btn btn--green'>
