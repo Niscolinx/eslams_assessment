93a8ea0cd675cbbcced0334e7e9e2537dd3e6beb
Igboanugwo Collins <munisco12@gmail.com>
2022-08-24 19:57:03 +0100

    added event cards

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,19 @@ export default function Document() {
         <Html>
             <Head />
             <title>eslams</title>
+            <link rel='preconnect' href='https://fonts.googleapis.com' />
+            <link
+                rel='preconnect'
+                href='https://fonts.gstatic.com'
+                crossOrigin='true'
+            />
+            <link
+                href='https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&family=Montserrat:wght@300;400;500;600;700&display=swap'
+                rel='stylesheet'
+            ></link>
             <body>
                 <Main />
                 <NextScript />
-              
             </body>
         </Html>
     )
