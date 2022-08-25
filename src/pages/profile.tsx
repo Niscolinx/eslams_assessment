1e5e0172b64f77afc62ce242f888dc42ba203a70
Igboanugwo Collins <munisco12@gmail.com>
2022-08-25 13:55:20 +0100

    working on profile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -8,7 +8,9 @@ function profile() {
                 <div className='profile__primary'>
                     <div className='profile__primary--picture'>
                         <div className='picture__cover'>
-                          <Image/>
+                          <Image src='/img/event.png' 
+                          width='100%'
+                          height='100%'/>
                         </div>
                         <div className='picture__avatar'>personal picture</div>
                     </div>
