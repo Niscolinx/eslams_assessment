1127ccec94600b898197df44df0b7fb26728270b
Igboanugwo Collins <munisco12@gmail.com>
2022-08-25 10:24:44 +0100

    added event cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -88,8 +88,8 @@ const Index = () => {
                                 </div>
                                 <Image
                                     src='/banner.png'
-                                    width='100%'
-                                    height='100%'
+                                    width='200px'
+                                    height='200px'
                                 />
                             </div>
                         </div>
