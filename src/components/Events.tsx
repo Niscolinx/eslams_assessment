00b5d58a1c81b83203119fdd6a91739e27ba50ae
Igboanugwo Collins <munisco12@gmail.com>
2022-08-25 06:46:57 +0100

    added event cards

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -70,98 +70,100 @@ const Event = ({
 function Events() {
     return (
         <div className='events'>
-            <div className="events__heading">
-                <h1 className='events__heading--text'> <span></span> Upcoming Events <span></span></h1>
+            <div className='events__heading'>
+                <span></span>{' '}
+                <h1 className='events__heading--text'>
+                    Upcoming Events</h1> <span></span>
+                <
             </div>
-            <div className="events__container">
-
-            <Event
-                heading='USA BASKETBALL SHOWCASE PRESENTED BY COINBASE'
-                price={1200}
-                details={[
-                    'Basketball, Water and Training Supplies',
-                    'Certfied Atletic trainers',
-                    'Availability of Shuttles',
-                    'NCAA Certified Checklist',
-                    'Waivers of Liability',
-                ]}
-                date={{
-                    from: '10th Sep',
-                }}
-                which={1}
-            />
-            <Event
-                heading='2022 FIBA AmeriCup for Men'
-                price={1500}
-                details={[
-                    'Basketball, Water and Training Supplies',
-                    'Certfied Atletic trainers',
-                    'Availability of Shuttles',
-                    'NCAA Certified Checklist',
-                    'Waivers of Liability',
-                ]}
-                date={{
-                    from: '5th Oct',
-                    to: '9th Oct',
-                }}
-                which={2}
-            />
-            <Event
-                heading='FIBA 3x3 U23 World Cup (women)'
-                price={750}
-                details={[
-                    'Basketball, Water and Training Supplies',
-                    'Certfied Atletic trainers',
-                    'Availability of Shuttles',
-                    'NCAA Certified Checklist',
-                    'Waivers of Liability',
-                ]}
-                date={{ from: '2nd Sep', to: '11th Sep' }}
-                which={3}
-            />
-            <Event
-                heading='2022 USA Basketball Gold Camp (boys)'
-                price={1500}
-                details={[
-                    'Basketball, Water and Training Supplies',
-                    'Certfied Atletic trainers',
-                    'Availability of Shuttles',
-                    'NCAA Certified Checklist',
-                    'Waivers of Liability',
-                ]}
-                date={{
-                    from: '4th Sep',
-                    to: '5th Sep',
-                }}
-                which={1}
-            />
-            <Event
-                heading='2022 USA Basketball Coach Academy'
-                price={1500}
-                details={[
-                    'Basketball, Water and Training Supplies',
-                    'Certfied Atletic trainers',
-                    'Availability of Shuttles',
-                    'NCAA Certified Checklist',
-                    'Waivers of Liability',
-                ]}
-                date={{ from: '10th Sep' }}
-                which={2}
-            />
-            <Event
-                heading='2022 USA Basketball Gold Camp (girls)'
-                price={1500}
-                details={[
-                    'Basketball, Water and Training Supplies',
-                    'Certfied Atletic trainers',
-                    'Availability of Shuttles',
-                    'NCAA Certified Checklist',
-                    'Waivers of Liability',
-                ]}
-                date={{ from: '3rd Sep', to: '4th Sep' }}
-                which={3}
-            />
-        </div>
+            <div className='events__container'>
+                <Event
+                    heading='USA BASKETBALL SHOWCASE PRESENTED BY COINBASE'
+                    price={1200}
+                    details={[
+                        'Basketball, Water and Training Supplies',
+                        'Certfied Atletic trainers',
+                        'Availability of Shuttles',
+                        'NCAA Certified Checklist',
+                        'Waivers of Liability',
+                    ]}
+                    date={{
+                        from: '10th Sep',
+                    }}
+                    which={1}
+                />
+                <Event
+                    heading='2022 FIBA AmeriCup for Men'
+                    price={1500}
+                    details={[
+                        'Basketball, Water and Training Supplies',
+                        'Certfied Atletic trainers',
+                        'Availability of Shuttles',
+                        'NCAA Certified Checklist',
+                        'Waivers of Liability',
+                    ]}
+                    date={{
+                        from: '5th Oct',
+                        to: '9th Oct',
+                    }}
+                    which={2}
+                />
+                <Event
+                    heading='FIBA 3x3 U23 World Cup (women)'
+                    price={750}
+                    details={[
+                        'Basketball, Water and Training Supplies',
+                        'Certfied Atletic trainers',
+                        'Availability of Shuttles',
+                        'NCAA Certified Checklist',
+                        'Waivers of Liability',
+                    ]}
+                    date={{ from: '2nd Sep', to: '11th Sep' }}
+                    which={3}
+                />
+                <Event
+                    heading='2022 USA Basketball Gold Camp (boys)'
+                    price={1500}
+                    details={[
+                        'Basketball, Water and Training Supplies',
+                        'Certfied Atletic trainers',
+                        'Availability of Shuttles',
+                        'NCAA Certified Checklist',
+                        'Waivers of Liability',
+                    ]}
+                    date={{
+                        from: '4th Sep',
+                        to: '5th Sep',
+                    }}
+                    which={1}
+                />
+                <Event
+                    heading='2022 USA Basketball Coach Academy'
+                    price={1500}
+                    details={[
+                        'Basketball, Water and Training Supplies',
+                        'Certfied Atletic trainers',
+                        'Availability of Shuttles',
+                        'NCAA Certified Checklist',
+                        'Waivers of Liability',
+                    ]}
+                    date={{ from: '10th Sep' }}
+                    which={2}
+                />
+                <Event
+                    heading='2022 USA Basketball Gold Camp (girls)'
+                    price={1500}
+                    details={[
+                        'Basketball, Water and Training Supplies',
+                        'Certfied Atletic trainers',
+                        'Availability of Shuttles',
+                        'NCAA Certified Checklist',
+                        'Waivers of Liability',
+                    ]}
+                    date={{ from: '3rd Sep', to: '4th Sep' }}
+                    which={3}
+                />
+            </div>
         </div>
     )
 }
