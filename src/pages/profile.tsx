3d44d910e0f4a47875c275fb28dc3772a5e300ca
Igboanugwo Collins <munisco12@gmail.com>
2022-08-25 13:29:54 +0100

    added footer

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -3,19 +3,18 @@ import React from 'react'
 function profile() {
     return (
         <div className='profile'>
-            <div className='profile__primary'>
+            <div className='profile__box'>
+                <div className='profile__primary'>
+                    <div className='profile__primary--picture'>
+                        <div className='picture__cover'>cover picture</div>
+                        <div className='picture__avatar'>personal picture</div>
+                    </div>
 
-              <div className="profile__primary--picture">
-                <div className="picture__cover">cover picture</div>
-                <div className="picture__avatar">personal picture</div>
-              </div>
+                    <div className='profile__primary--details'>Form</div>
+                </div>
 
-              <div className="profile__primary--details">
-                Form
-              </div>
+                <div className='profile__secondary'>The Events</div>
             </div>
-
-            <div className='profile__secondary'>The Events</div>
         </div>
     )
 }
