080320c564aad4e42b629af852216b365532f567
Igboanugwo Collins <munisco12@gmail.com>
2022-08-26 12:08:45 +0100

    working on otp

--- src/pages/api/auth/authContext.tsx
+++ src/pages/api/auth/authContext.tsx
@@ -1,8 +1,16 @@
 import {createContext} from 'react';
 
+
 type ContextType = {
-    isVerified: boolean,
-    setIsVerified: (isVerified: boolean) => void
+    gender: boolean,
+    setGender: (gender: boolean) => void
+    password: boolean,
+    setPassword: (password: boolean) => void
+    phoneNumber: boolean,
+    setPhoneNumber: (phoneNumber: boolean) => void
+    birthDate: boolean,
+    setBirthDate: (birthDate: boolean) => void
+ 
 }
 
 export const AuthContext = createContext<ContextType>(null as any);
