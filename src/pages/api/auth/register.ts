acf47f93b4921c76175f82e8b7f2206e6f9c1b06
Igboanugwo Collins <munisco12@gmail.com>
2022-08-26 10:48:02 +0100

    working on otp

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -9,7 +9,7 @@ async function signupHandler(req: NextApiRequest, res: NextApiResponse) {
         await dbConnect()
 
         console.log('req body', req.body)
-        const {firstName, lastName, email, password,phonenumber, dateOfBirth, Gender, GuardianName, GuardianPhoneNumber, GuardianEmail, GuardianRelationship, institutionName, institutionType, institutionYearOfStudy } = req.body
+        const {firstName, lastName, email, password,phoneNumber, dateOfBirth, Gender, GuardianName, GuardianPhoneNumber, GuardianEmail, GuardianRelationship, institutionName, institutionType, institutionYearOfStudy } = req.body
         //Validate
         if (!email || !email.includes('@') || !password || !phoneNumber) {
             console.log('failed')
@@ -18,9 +18,7 @@ async function signupHandler(req: NextApiRequest, res: NextApiResponse) {
         }
 
         const existingUserEmail = await User.findOne({ email })
-        const existingUsername = await User.findOne({
-            username,
-        })
+        
         const existingPhoneNumber = await User.findOne({ phoneNumber })
 
 
@@ -30,71 +28,22 @@ async function signupHandler(req: NextApiRequest, res: NextApiResponse) {
             })
         }
 
-        if (existingUsername) {
-            return res.status(401).json({
-                message: 'Username already taken',
-            })
-        }
+       
 
         if (existingPhoneNumber) {
             return res.status(401).json({
                 message: 'Number already in use',
             })
         }
-        if (referral) {
-            const firstUpliner = await User.findOne({ username: referral })
-
-            if (!firstUpliner) {
-                return res.status(401).json({
-                    message: 'Referral not found',
-                })
-            } else {
-                firstUpliner.referrals.push({
-                    username,
-                    level: 1,
-                })
-
-                await firstUpliner.save()
-
-                if (firstUpliner.upliner) {
-                    const secondUpliner = await User.findOne({
-                        username: firstUpliner.upliner,
-                    })
-
-                    if (secondUpliner) {
-                        secondUpliner.referrals.push({
-                            username,
-                            level: 2,
-                        })
-
-                        await secondUpliner.save()
-
-                        if (secondUpliner.upliner) {
-                            const thirdUpliner = await User.findOne({
-                                username: secondUpliner.upliner,
-                            })
-
-                            if (thirdUpliner) {
-                                thirdUpliner.referrals.push({
-                                    username,
-                                    level: 3,
-                                })
-
-                                await thirdUpliner.save()
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        
 
         const storeUser = new User({
             email,
             password: await bcrypt.hash(password, 12),
-            username: username.split(' ').join(''),
+            firstName,
+            lastName,
             phoneNumber,
-            upliner: referral,
-            referralLink: `https://www.1960token.com/auth/register?reg=${username.split(' ').join('')}`,
+          ,
         })
 
         const verifyStored = await storeUser.save()
