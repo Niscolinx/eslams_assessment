66ae25d609a699052144dd93130571817e130d08
Igboanugwo Collins <munisco12@gmail.com>
2022-08-26 12:17:37 +0100

    working on register

--- src/components/stepForm/Checkout.tsx
+++ src/components/stepForm/Checkout.tsx
@@ -51,6 +51,7 @@ export default function Checkout() {
     const [loading, setLoading] = useState(false)
 
     const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
     const [gender, setGender] = useState('male')
     const [password, setPassword] = useState('')
     const [phoneNumber, setPhoneNumber] = useState('')
