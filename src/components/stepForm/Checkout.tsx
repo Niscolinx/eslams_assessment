26675a96e5afceaa782b6bfc251bb06ea419c74b
Igboanugwo Collins <munisco12@gmail.com>
2022-08-26 15:39:11 +0100

    working on register validation

--- src/components/stepForm/Checkout.tsx
+++ src/components/stepForm/Checkout.tsx
@@ -51,6 +51,21 @@ export default function Checkout() {
     const [activeStep, setActiveStep] = useState(0)
     const [loading, setLoading] = useState(false)
 
+    const [handleInput, setHandleInput] = useState<any>({
+        firstName: '',
+        lastName: '',
+        personalEmail: '',
+        phoneNumber: '',
+        password: '',
+        gender: '',
+        birthDate: null,
+        guardianEmail: '',
+        guardianPhoneNumber: '',
+        guardianName: '',
+        guardianRelationship: '',
+
+    })
+
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [personalEmail, setPersonalEmail] = useState('')
@@ -80,6 +95,13 @@ export default function Checkout() {
     type ValidationError = { key: string[]; message: { [key: string]: string } }
 
     const handleNext = () => {
+        if(){}
+
+
+
+
+
+
         //setActiveStep(activeStep + 1)
     }
 
