d7e1f14fbc7e8d45fb83b533226c37336a9a5d0b
Igboanugwo Collins <munisco12@gmail.com>
2022-08-27 22:22:17 +0100

    working on login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -126,11 +126,11 @@ function Footer() {
 }
 
 const Index = () => {
-    const router = useRouter()
+    // const router = useRouter()
 
-    useEffect(() => {
-        router.push('/auth/register')
-    }, [])
+    // useEffect(() => {
+    //     router.push('/auth/register')
+    // }, [])
 
     return (
         <div className='marketplace'>
@@ -177,21 +177,21 @@ const Index = () => {
 
 export default Index
 
-// export async function getServerSideProps(
-//     context: GetSessionParams | undefined
-// ) {
-//     const session = await getSession(context)
+export async function getServerSideProps(
+    context: GetSessionParams | undefined
+) {
+    const session = await getSession(context)
 
-//     if (!session) {
-//         return {
-//             redirect: {
-//                 destination: '/auth/login',
-//                 permanent: false,
-//             },
-//         }
-//     }
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/auth/register',
+                permanent: false,
+            },
+        }
+    }
 
-//     return {
-//         props: { session },
-//     }
-// }
+    return {
+        props: { session },
+    }
+}
