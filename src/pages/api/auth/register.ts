8023ed54deb22c2ba6d11973904740dd0953a8f2
Igboanugwo Collins <munisco12@gmail.com>
2022-08-27 11:16:06 +0100

    working on validation

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -4,9 +4,7 @@ import bcrypt from 'bcryptjs'
 import dbConnect from '../../../lib/dbConnect'
 import User from '../../../models/User'
 import { transporter } from '../../../utils/emailTransport'
-import msdf from 'mjml'
 import mjml2html from 'mjml'
-
 
 async function signupHandler(req: NextApiRequest, res: NextApiResponse) {
     try {
@@ -14,9 +12,23 @@ async function signupHandler(req: NextApiRequest, res: NextApiResponse) {
 
         //console.log('req body', req.body)
         console.log('register', req.body)
-        const {firstName, lastName, personalEmail, password,phoneNumber, birthDate, Gender, GuardianName, GuardianPhoneNumber, GuardianEmail, GuardianRelationship, institutionName, institutionType, institutionYearOfStudy } = req.body
+        const {
+            firstName,
+            lastName,
+            personalEmail,
+            password,
+            phoneNumber,
+            birthDate,
+            Gender,
+            GuardianName,
+            GuardianPhoneNumber,
+            GuardianEmail,
+            GuardianRelationship,
+            institutionName,
+            institutionType,
+            institutionYearOfStudy,
+        } = req.body
 
-    
         //Validate
         // if (!personalEmail || !personalEmail.includes('@') || !password || !phoneNumber) {
         //     console.log('failed')
@@ -25,9 +37,8 @@ async function signupHandler(req: NextApiRequest, res: NextApiResponse) {
         // }
 
         const existingEmail = await User.findOne({ personalEmail })
-        
-        const existingPhoneNumber = await User.findOne({ phoneNumber })
 
+        const existingPhoneNumber = await User.findOne({ phoneNumber })
 
         if (existingEmail) {
             return res.status(401).json({
@@ -35,14 +46,11 @@ async function signupHandler(req: NextApiRequest, res: NextApiResponse) {
             })
         }
 
-       
-
         if (existingPhoneNumber) {
             return res.status(401).json({
                 message: 'Number already in use',
             })
         }
-        
 
         const storeUser = new User({
             email: personalEmail,
@@ -58,8 +66,7 @@ async function signupHandler(req: NextApiRequest, res: NextApiResponse) {
             GuardianRelationship,
             institutionName,
             institutionType,
-            institutionYearOfStudy 
-          
+            institutionYearOfStudy,
         })
 
         const verifyStored = await storeUser
@@ -70,53 +77,65 @@ async function signupHandler(req: NextApiRequest, res: NextApiResponse) {
 
         //generate random 6 digit code
         const otp = Math.floor(Math.random() * 1000000)
-        
-         console.log({otp})
 
-         const htmlOutput = mjml2html(
-             `
+        console.log({ otp })
+
+        const htmlOutput = mjml2html(
+            `
   <mjml>
-    <mj-body>
-      <mj-section>
-        <mj-column>
-          <mj-text>
-            Hello World!
-          </mj-text>
-        </mj-column>
-      </mj-section>
-    </mj-body>
-  </mjml>
+  <mj-body>
+    <mj-section background-color='#E8E7E7' border-radius='1rem' font-family='Lato'>
+      <mj-column>
+
+
+        <mj-image width="100px" src="https://i.ibb.co/FXyQXg8/logo.jpg"></mj-image>
+
+        <mj-divider border-color="#112ea3" border-width='1px'></mj-divider>
+
+        <mj-text font-size='20px' align='center'>Hello Igboanugwo Collins</mj-text>
+
+
+
+        <mj-text font-size='15px' align='center'>Below is your Vefirication Code</mj-text>
+
+        <mj-button font-size='20px' color='black' background-color='white' padding-top='30px'>582328</mj-button>
+
+
+      </mj-column>
+    </mj-section>
+  </mj-body>
+</mjml>
 `,
-             options
-         )
-       
-         const mail = {
-             from: 'admin@eslams.com',
-             to: 'munisco12@gmail.com',
-             subject: `Account Verification`,
-             html: `<h1>Your OTP</h1></br> <p>${otp}</p>`,
-         }
+            {}
+        )
 
-         transporter.verify().then((data) => {
-            console.log('verified email credentials', data)
-         }).catch(err => console.log('not verified email'))
+        const mail = {
+            from: 'admin@eslams.com',
+            to: 'munisco12@gmail.com',
+            subject: `Account Verification`,
+            html: `<h1>Your OTP</h1></br> <p>${otp}</p>`,
+        }
 
-         transporter.sendMail(mail, (err, data) => {
-             if (err) {
-                 console.log({ err })
-                 res.json({
-                     status: 'fail',
-                 })
-             } else {
-                 console.log('email sent', data)
-                 res.json({
-                     status: 'success',
-                 })
-             }
-         })
+        transporter
+            .verify()
+            .then((data) => {
+                console.log('verified email credentials', data)
+            })
+            .catch((err) => console.log('not verified email'))
 
-
-       
+        transporter.sendMail(mail, (err, data) => {
+            if (err) {
+                console.log({ err })
+                res.json({
+                    status: 'fail',
+                })
+            } else {
+                console.log('email sent', data)
+                res.json({
+                    status: 'success',
+                })
+            }
+        })
     } catch (err) {
         console.log({ err })
     }
