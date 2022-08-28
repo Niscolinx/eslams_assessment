dff38ec70b23aee63a45a4ef06d20f84fc37fbe7
Igboanugwo Collins <munisco12@gmail.com>
2022-08-28 13:23:56 +0100

    working on env variables

---
+++ src/pages/api/checkAuth.ts
@@ -0,0 +1,13 @@
+import { IUser } from '../../models/User'
+import { NextApiRequest, NextApiResponse } from 'next'
+import User from '../../models/User'
+import dbConnect from '../../lib/dbConnect'
+
+export default async function checkAuth(req: NextApiRequest, res: NextApiResponse) {
+    await dbConnect()
+    const { email }: IUser = req.body.user
+
+    const user = await User.findOne({ email })
+
+    return res.json(user)
+}
