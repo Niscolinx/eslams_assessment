d70fb01f928c75bd24a813bd6bf80eb3716573af
Igboanugwo Collins <munisco12@gmail.com>
2022-08-28 15:45:18 +0100

    working on env variables

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -42,7 +42,7 @@ const Layout: React.FC<{}> = ({ children }) => {
             <div
             >
                 <NavContext.Provider value={navContext}>
-                    <Nav session={session} />
+                    {/* <Nav session={session} /> */}
 
                     <main className='mx-auto max-w-[1500px] overflow-hidden'>{children}</main>
                     {/* <Footer hideFooter={hideFooter} /> */}
