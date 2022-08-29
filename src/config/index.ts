481d0ff590994beb9f4995da1df2cdb341433e86
Igboanugwo Collins <munisco12@gmail.com>
2022-08-29 20:30:00 +0100

    merged dev

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV === 'production'
 
-export const serverUrl = dev ? 'https://www.1960token.com' : 'http://localhost:3000'
+export const serverUrl = dev ? 'https://eslams-assessment.vercel.app' : 'http://localhost:3000'
