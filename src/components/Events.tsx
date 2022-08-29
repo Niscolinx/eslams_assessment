55fa6eb5cd2818050208329516a8073c7ead62e6
Igboanugwo Collins <munisco12@gmail.com>
2022-08-29 07:24:06 +0100

    added jose and removed jwt as tokens verifier

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -139,10 +139,10 @@ const Event = ({
                         details.map((item, i) => {
                             const [key, value] = Object.entries(item)[0]
 
-                            console.log(value)
+                            console.log(typeof value, value)
                             return (
                                 <ul key={i}>
-                                    <li><span>{key}</span>: <span>{ 1 ? 'hee': value}</span></li>
+                                    <li><span>{key}</span>: <span>{ typeof value === Object ? 'hee': value}</span></li>
                                 </ul>
                             )})}
                     <ul></ul>
