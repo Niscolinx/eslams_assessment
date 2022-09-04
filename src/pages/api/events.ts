932c997f498999f9bdaff10ae663206104c69a78
Igboanugwo Collins <munisco12@gmail.com>
2022-09-04 19:28:57 +0100

    working on filter

--- src/pages/api/events.ts
+++ src/pages/api/events.ts
@@ -1,26 +1,19 @@
-import { EVENTDATA } from './../../components/Events';
-import { NextApiRequest, NextApiResponse} from 'next';
-import dbConnect from '../../lib/dbConnect';
-import Event from '../../models/event';
+import { EVENTDATA } from './../../components/Events'
+import { NextApiRequest, NextApiResponse } from 'next'
+import dbConnect from '../../lib/dbConnect'
+import Event from '../../models/event'
 
-
-export default async function events(req: NextApiRequest, res: NextApiResponse){
-
-    
-    
-
+export default async function events(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
     try {
         await dbConnect()
 
         const event = await Event.find({})
-            
-       console.log({event})
 
+        res.status(200).json(event)
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong' })
     }
-    catch (error) {
-        res.status(500).json({message: 'Something went wrong'})
-    }
-
-  
-
 }
