ac22c689130d4a94e5a870c8d937552cebe73a87
Igboanugwo Collins <munisco12@gmail.com>
2022-09-04 18:27:38 +0100

    working on filter

--- src/pages/api/events.ts
+++ src/pages/api/events.ts
@@ -7,7 +7,34 @@ export default async function events(req: NextApiRequest, res: NextApiResponse){
     
     const {EVENTDATA} = req.body
 
-    const storeEvents = await new Event(EVENTDATA).save()
+
+    try {
+
+        EVENTDATA.forEach(async (event: any) => {
+            console.log(event)
+            const newEvent = new Event({
+                heading: event.heading,
+                price: event.price,
+                details: [{
+                    age: event.details[0].Age,
+                    competitionType: event.details[0]['Competition Type'],
+                    location: event.details[0].location,
+                    registrationRequirements: event.details[0]['Registration Requirements']
+                }],
+                date: {
+                    from: event.date.from,
+                    to: event.date.to
+                },
+
+
+            })
+            await newEvent.save()
+        })
+
+    }
+    catch (error) {
+        res.status(500).json({message: 'Something went wrong'})
+    }
 
     return res.status(200).json({
         message: 'stored'
