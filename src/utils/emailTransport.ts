469b4e9d96dc0b25e71370b4183581c88061fd6d
Igboanugwo Collins <munisco12@gmail.com>
2022-09-08 14:31:45 +0100

    working on updating photo upload

--- src/utils/emailTransport.ts
+++ src/utils/emailTransport.ts
@@ -15,8 +15,36 @@ import nodeMailer from 'nodemailer'
       },
   })
 
-const getPhotoUrl = selector => {
-    
+export const getPhotoUrl = (selector:string) => {
+
+    const selectPhoto = document.querySelector(selector)
+
+    if(!selectPhoto) return 'no photo selected'
+
+    const photo = selectPhoto as HTMLInputElement
+
+    const photoUrl = new Promise((resolve, reject) => {
+
+        photo.addEventListener('change', (e) => {
+
+            const file = e.target as HTMLInputElement
+
+            const reader = new FileReader()
+
+            reader.readAsDataURL(file.files![0])
+
+            reader.onload = () => resolve(reader.result)
+
+            reader.onerror = (err) => reject(err)
+
+        })
+
+    })
+
+    return photoUrl
+
+
+
 }
 
 
