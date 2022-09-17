663e60ff25301202de5a92f6644ab28f10578959
Igboanugwo Collins <munisco12@gmail.com>
2022-09-17 19:13:40 +0100

    modified store

--- src/store/app/hooks.ts
+++ src/store/app/hooks.ts
@@ -1,6 +1,6 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
-import type { AppDispatch, AppState } from './store'
+import type { AppState } from './store'
 
 export const useAppDispatch = () => useDispatch
 
